ea27c42a71ab79d6bb7f224541c6c497974dfad4
brunosag <brunosag02@gmail.com>
2022-06-22 08:20:56 -0300

    adjust character limits

--- website/models.py
+++ website/models.py
@@ -4,23 +4,23 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
     workouts = db.relationship("Workout")
 
 
 class Workout(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    description = db.Column(db.String(100))
+    name = db.Column(db.String(12))
+    description = db.Column(db.String(18))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     exercises = db.relationship("Exercise")
 
 
 class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    weight = db.Column(db.Float)
-    details = db.Column(db.String(15))
+    name = db.Column(db.String(45))
+    weight = db.Column(db.Float(5))
+    details = db.Column(db.String(5))
     include_details = db.Column(db.Boolean)
     workout_id = db.Column(db.Integer, db.ForeignKey("workout.id"))

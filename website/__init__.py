da3e4eae1ba33d9aa05e7fc0acf18ba930d3582e
brunosag <brunosag02@gmail.com>
2023-04-21 00:45:32 -0300

    remove next parameter after unauthorized redirect

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -8,12 +8,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.environ.get(
-        'SECRET_KEY', default='006363ce276b892f9f89d16571fd0113'
-    )
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-        'SQLALCHEMY_DATABASE_URI', default='sqlite:///db.sqlite3'
-    )
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", default="006363ce276b892f9f89d16571fd0113")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI", default="sqlite:///db.sqlite3")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
@@ -21,8 +17,8 @@ def create_app():
     from .auth import auth
     from .views import views
 
-    app.register_blueprint(auth, url_prefix='/')
-    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(views, url_prefix="/")
 
     with app.app_context():
         db.create_all()
@@ -37,5 +33,9 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
+    @login_manager.unauthorized_handler
+    def unauthorized():
+        return redirect(url_for("auth.signin"))
 
     return app
